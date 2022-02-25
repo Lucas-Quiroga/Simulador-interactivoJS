1ae06892a41fd9ee219e71461486a1cb6cbae5f0
Lucas Quiroga <quiroga.lucasoffice@gmail.com>
2022-02-25 16:38:57 -0300

    Proyecto terminado

--- main.js
+++ main.js
@@ -1,67 +1,60 @@
+// Mi idea de proyecto es armar una aplicación web en donde se le da un formulario a la persona interesada para que pueda seleccionar el servicio que desee y la cantidad de turnos que la misma precise//
 
-//Declaro los precios de los servicios//
+//El esqueleto de HTML lo prepare simple y le agregue un poco de CSS para no dejarlo TAN basico//
+
+//Declaro el valor de los servicios//
 let tarot = 1600;
 let reiki = 1100;
 let limpiezaEnergetica = 1100;
 let sanacion = 700;
-let tallerTarot = 1500;
-let tallerCalenMaya = 1300;
-let tallerRunas = 1200;
 
-/*Posible ajuste para que sea todo en mayuscula
-let pregunta = dato1.toUpperCase;*/
-
-let nombreServicio = prompt("Escriba el servicio que desee\n Opciones: Lectura de tarot, Reiki, Limpieza energetica, Sanación");
-let turno = prompt("Escriba cuantos turnos desea agendar");
-
-
+//Funcion que toma el servicio y lo multiplica por el turno o los turnos elegidos//
 function seleccionServic (nombreServicio, turno){
-    //Cree un switch para evaluar el caso particular de cada servicio
         switch (nombreServicio) {
         case "Lectura de tarot":
         case "Tarot":
-            return console.log (`El total del servicio tendría un valor de $${tarot * turno}`);
+            return `El total del servicio tendría un valor de $${tarot * turno}`;
             break;
         case "Reiki":
         case "reiki":
-            return console.log (`El total del servicio tendría un valor de $${reiki * turno}`);
+            return `El total del servicio tendría un valor de $${reiki * turno}`;
             break;
         case "Limpieza energetica":
         case "limpieza energetica":
-            return console.log (`El total del servicio tendría un valor de $${limpiezaEnergetica * turno}`);
+            return `El total del servicio tendría un valor de $${limpiezaEnergetica * turno}`;
             break;
         case "Sanación":
         case "sanación":
-            return console.log (`El total del servicio tendría un valor de $${sanacion * turno}`);
+            return `El total del servicio tendría un valor de $${sanacion * turno}`;
             break;
         default:
-            return 0;
+            return `Error: No eligió ningun servicio ofrecido`;
             break;
     }
 }
 
-seleccionServic(nombreServicio,turno);
-
-let cantidadDeTurnos
-
-for(let i= 0; i <= 30; i++){
-
-}
-
-let dia = prompt("Escriba el día que usted puede comunicarse \n Opciones: Martes o Jueves")
-let mes = prompt("Escriba el mes de preferencia \n Opciones: Marzo o Abril")
-let hora = prompt("Escriba la hora de preferencia \n Opciones: de 14hs a 21hs")
-
-
-function turnoDado(dia,mes,hora){
+//Funcion que toma en particular los datos sobre el encuentro virtual//
+//Dato: los número de fechas corresponder a los días martes y jueves del mes de marzo 2022, no puse los de abril sino sería mas largo, en resumen de ejemplo//
+function turnoDado(dia,fecha,mes,hora){
     let horarioInicio = 14;
     let horarioFinal = 21;
-
-    if((dia == "Martes" || "Jueves") && (mes == "Marzo" || "Abril") && (hora >= horarioInicio && hora <= horarioFinal)) {
-        return `Su turno es el día ${dia} de ${mes} a las ${hora}hs`;
+    if((dia == "Martes" || "Jueves") && (fecha == 1 || 3 || 8 || 10 || 15 || 17 || 22 || 24 || 29 || 31) && (mes == "Marzo" || "Abril") && (hora >= horarioInicio && hora <= horarioFinal)) {
+        return `Su turno será el día ${dia} ${fecha} de ${mes} a las ${hora}hs`;
     } else {
-        return console.log("Por favor introduzca bien la información")
+        return `Por favor introduzca bien la información solicitada.`;
     }
 }
 
-turnoDado(dia,mes,hora);
+//Le preguntamos al usuario sobre sus preferencias//
+let nombreServicio = prompt("Escriba el servicio que desee\n Opciones: Lectura de tarot, Reiki, Limpieza energetica, Sanación");
+let turno = prompt("Escriba cuantos turnos desea agendar");
+let dia = prompt("Escriba el día que usted puede comunicarse \n Opciones: Martes o Jueves");
+let fecha = parseInt(prompt("Escriba la fecha que usted puede comunicarse \n Opciones: 1 - 3 - 8 - 10 - 15 - 17 - 22 - 24 - 29 - 31"));
+let mes = prompt("Escriba el mes de preferencia \n Opciones: Marzo o Abril");
+let hora = parseInt(prompt("Escriba la hora de preferencia \n Opciones: de 14hs a 21hs"));
+
+//Guardamos las funciones dentro de las variables, las llamamos y las mostramos por el alert//
+let servicioMasTurno = seleccionServic(nombreServicio, turno);
+let fechaYHorario = turnoDado(dia,fecha,mes,hora);
+alert(`${servicioMasTurno} y ${fechaYHorario} `);
+alert("Muchas gracias! \n Ante cualquier duda o consulta comunicarse al número +15 12345698");
